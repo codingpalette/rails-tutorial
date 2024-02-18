0bc1ab89b459c6021666dddff430d19b6de02d65
codingpalette <codingpalette@gmail.com>
2024-02-18 12:42:31 +0900

    crud 작업

--- config/routes.rb
+++ config/routes.rb
@@ -12,10 +12,7 @@ end
 
 
 Rails.application.routes.draw do
-    root "articles#index"
+  root "articles#index"
 
-    get "/articles", to: "articles#index"
-     get "/articles/:id", to: "articles#show"
-
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :articles
 end
